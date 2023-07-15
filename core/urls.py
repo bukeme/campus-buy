e4a104c786bb0ea9c9414322f014372e10cc12f0
Ukeme Bassey <basseyukemme@gmail.com>
2023-07-15 14:13:15 +0100

    updated settings

--- core/urls.py
+++ core/urls.py
@@ -35,5 +35,5 @@ urlpatterns = [
     ), name='openapi-schema'), 
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
